a0f929b2e2a0faabdccdd0f75fbd65dd60187da4
WillOli <William.ika@gmail.com>
2022-12-27 10:50:13 -0300

    more exercises

---
+++ Codes/Day 2/23.Interactive-Conding-exercise-BMI-Calculator.py
@@ -0,0 +1,9 @@
+# Don't change the code below
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+
+# Write your code below this line
+result1 = int(weight)
+result2 = float(height)
+
+print(int(result1/result2**2))
