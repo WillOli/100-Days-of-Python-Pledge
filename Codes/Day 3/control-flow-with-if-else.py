3b1ed8e5399b14153d19616706632f4ced12133e
WillOli <William.ika@gmail.com>
2023-01-23 09:56:07 -0300

    Day 3

---
+++ Codes/Day 3/control-flow-with-if-else.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!!")
+
+heigth = int(input("What is your height in cm? "))
+
+if heigth >= 120:
+    print("You can ride the rollercoaster! ")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
