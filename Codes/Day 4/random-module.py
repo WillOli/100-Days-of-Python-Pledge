23dc785b692d32d93861fb89730bf3a63ce391d2
WillOli <William.ika@gmail.com>
2023-02-07 10:53:44 -0300

    more exercise day 4

--- Codes/Day 4/random-module.py
+++ Codes/Day 4/random-module.py
@@ -2,10 +2,13 @@ import random
 import my_module
 random_intetger = random.randint(1, 10)
 
-# print(my_module.pi)
 
-random_float = random.random()
+print(random_intetger)
+
+random_float = random.random() * 5
 print(random_float)
+
+# aleatório de dois inteiros
 
 love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}")
